bfc77cfe5524f089633dfe34e9c9ccf0fa27d65b
신예지 <gjsld1@naver.com>
2019-08-23 01:54:06 +0900

    character recognition & parsing

--- venv/test.py
+++ venv/test.py
@@ -1,13 +1,31 @@
 import cv2
+import pytesseract
+from PIL import Image
+
+from bs4 import BeautifulSoup
 
 
-#cv2.IMREAD_COLOR : 투명한 부분 무시되는 컬
-#cv2.IMREAD_GRAYSCALE : 흑백 이미지로 로
-#cv2.IMREAD_UNCHANGED : 알파 채컬을 포함한 이미지 그대로 로드
+#cv2.IMREAD_COLOR : 투명한 부분 무시되는 컬러
+#cv2.IMREAD_GRAYSCALE : 흑백 이미지로 로드
+#cv2.IMREAD_UNCHANGED : 알파 채널을 포함한 이미지 그대로 로드
+
+hocr = pytesseract.image_to_pdf_or_hocr('banana.jpeg', extension='hocr')
+
+soup = BeautifulSoup(hocr, 'html.parser')
+#print(soup)
+
+coordinates = soup.select('span.ocrx_word')
+print(coordinates)
+"""
+print(pytesseract.image_to_string(Image.open('banana.jpeg')))
+
 src = cv2.imread('ginja.png', cv2.IMREAD_COLOR)
+src2 = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+cv2.imshow('src2', src2)
 dst = cv2.imread(src, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('src', src)
-cv2.imshow('dst', dst)
+#cv2.imshow('dst', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
